3cb600dce8098f367fad6d9fedd1f7c46e9df9fe
lightnovelhub <112533991+lightnovelhub@users.noreply.github.com>
2022-09-25 15:53:11 +0530

    Update BookmarkServer.py

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python3
-#
-# A *bookmark server* or URI shortener.
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+
 import http.server
 import os
 from urllib.parse import unquote
 from socketserver import ThreadingMixIn
 from urllib.parse import urlparse
+import youtube_dl
+import json
 memory = {}
-chrome_options = Options()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_path=os.environ.get(
-    "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
 class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
@@ -36,26 +23,21 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             query = urlparse(self.path).query
             query_components = dict(qc.split("=") for qc in query.split("&"))
             searchQuery = query_components["query"]
-            myquery = "https://www.y2mate.com/youtube-mp3/" + searchQuery
-            browser.get(myquery)
-            WebDriverWait(browser, delay).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'caption')))
-            l = browser.find_element(
-                By.CSS_SELECTOR, "#result > div > div.col-xs-12.col-sm-7.col-md-8 > div > div > div.btn-group.inline.p-r-md > button.btn.btn-default.dropdown-toggle")
-            l.click()
-            browser.find_element(By.CSS_SELECTOR, "#result > div > div.col-xs-12.col-sm-7.col-md-8 > div > div > div.btn-group.inline.p-r-md.open > div > div.panel-body.panel_file_type > div.col-xs-5.p-t-md > ul > li:nth-child(1) > a").click()
-            browser.find_element(By.CSS_SELECTOR, "#process_mp3").click()
-            WebDriverWait(browser, delay).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '#process-result > div')))
-            url = browser.find_element(
-                By.CSS_SELECTOR, "#process-result > div > a").get_attribute("href")
-            self.wfile.write(bytes(url, "utf-8"))
-        except TimeoutException:
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            print("Loading took too much time!")
-            self.wfile.write(bytes("Failed", "utf-8"))
+            ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+
+            with ydl:
+                result = ydl.extract_info(
+                    'http://www.youtube.com/watch?v='+ query_components["query"],
+                    download=False  # We just want to extract the info
+                )
+
+            if 'entries' in result:
+                # Can be a playlist or a list of videos
+                video = result['entries'][0]
+            else:
+                # Just a video
+                video = result
+            self.wfile.write(bytes(json.dumps(video), "utf-8"))
 
 
 if __name__ == '__main__':
