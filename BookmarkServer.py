89780b539514a2090d71b1c458af1aebea5e685a
lightnovelhub <112533991+lightnovelhub@users.noreply.github.com>
2022-09-25 15:58:36 +0530

    Update BookmarkServer.py

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,4 +1,3 @@
-
 import http.server
 import os
 from urllib.parse import unquote
@@ -15,29 +14,29 @@ class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
 
 class Shortener(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        try:
-            delay = 3  # seconds
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            query = urlparse(self.path).query
-            query_components = dict(qc.split("=") for qc in query.split("&"))
-            searchQuery = query_components["query"]
-            ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+        delay = 3  # seconds
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        searchQuery = query_components["query"]
+        ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 
-            with ydl:
-                result = ydl.extract_info(
-                    'http://www.youtube.com/watch?v='+ query_components["query"],
-                    download=False  # We just want to extract the info
-                )
+        with ydl:
+            result = ydl.extract_info(
+                'http://www.youtube.com/watch?v=' +
+                query_components["query"],
+                download=False  # We just want to extract the info
+            )
 
-            if 'entries' in result:
-                # Can be a playlist or a list of videos
-                video = result['entries'][0]
-            else:
-                # Just a video
-                video = result
-            self.wfile.write(bytes(json.dumps(video), "utf-8"))
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
+        else:
+            # Just a video
+            video = result
+        self.wfile.write(bytes(json.dumps(video), "utf-8"))
 
 
 if __name__ == '__main__':
